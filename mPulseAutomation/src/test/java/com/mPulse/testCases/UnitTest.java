2ed7bd12abfd56f220e5fd9166f2471b801f536a
Krishna Nandan Yadav <krishna.yadav@quovantis.com>
2020-03-30 17:13:46 +0530

    some changes in file

--- mPulseAutomation/src/test/java/com/mPulse/testCases/UnitTest.java
+++ mPulseAutomation/src/test/java/com/mPulse/testCases/UnitTest.java
@@ -1,15 +1,34 @@
 package com.mPulse.testCases;
 
-import java.util.ArrayList;
+import org.openqa.selenium.WebDriver;
+import org.testng.annotations.Parameters;
+import org.testng.annotations.Test;
 
-import com.mPulse.factories.mPulseDB;
-import com.mPulse.utility.AudienceApi;
+import com.mPulse.factories.BrowserFactory;
 
-public class UnitTest {
+public class UnitTest extends BaseTest{
 	
-	public static void main(String[] args) throws Exception {
-		ArrayList<String> audience_ids = mPulseDB.getAllRowData("select id from audience_member where deleted = false and mobile_phone is not null and account_id=1580 limit 5");
-		AudienceApi.deleteAllMemberById(audience_ids);
-		System.out.println("data is - "+audience_ids);
+	@Test(groups = { "sanity_krishna" })
+	@Parameters({ "browser" })
+	public void test_one(String browser) {
+		WebDriver driver = BrowserFactory.getBrowser(browser);
+		loginTo_mPulse_cp(driver);
+		String title = driver.getTitle();
+		String url = driver.getCurrentUrl();
+		
+		System.out.println("**************** Current url is - "+url);
+		System.out.println("**************** Current title is - "+title);
+	}
+	
+	@Test(groups = { "sanity_krishna_headless" })
+	@Parameters({ "browser" })
+	public void test_one_2(String browser) {
+		WebDriver driver = BrowserFactory.getBrowser(browser);
+		loginTo_mPulse_cp(driver);
+		String title = driver.getTitle();
+		String url = driver.getCurrentUrl();
+		
+		System.out.println("**************** Current url is - "+url);
+		System.out.println("**************** Current title is - "+title);
 	}
 }
